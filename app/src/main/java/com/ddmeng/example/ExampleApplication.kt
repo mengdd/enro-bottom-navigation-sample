04cca89409089cbede59503bdd41c3004f98161c
Dandan Meng <mengdd@users.noreply.github.com>
2022-01-12 00:04:21 +0800

    Initial commit

---
+++ app/src/main/java/com/ddmeng/example/ExampleApplication.kt
@@ -0,0 +1,15 @@
+package com.ddmeng.example
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class ExampleApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
